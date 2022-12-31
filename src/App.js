f6686667769e91affabd82388932d7af22888fb3
Jake <jakezcanada@gmail.com>
2022-12-31 17:20:19 -0500

    Create Navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import MyNav from "./components/Navbar";
 
 function App() {
   return (
     <div className="App">
+      <MyNav />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
