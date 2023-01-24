bf40247910cf4d705772ca08c00fdd9fe4256832
Peterson Guo <petersonguo@gmail.com>
2023-01-24 17:26:36 -0500

    Merge

--- src/components/pages/Quiz/Quiz.js
+++ src/components/pages/Quiz/Quiz.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Prompt from "./Prompt";
 import SettingsSidebar from "./SettingsSidebar";
 import HistorySidebar from "./HistorySidebar";
-import questions from './questions.json';
+import questions from '../../../questions.json';
 import {ChevronLeftIcon, ChevronRightIcon, Cog8ToothIcon} from "@heroicons/react/24/outline";
 import {motion, useCycle} from "framer-motion";
 
@@ -13,7 +13,7 @@ export default function Quiz() {
   const [index, setIndex] = React.useState(-1);
   const [question, setQuestion] = React.useState(getQuestion);
   const [isVisible, onCycle] = useCycle(false, true);
-  const [animate, cycle] = useCycle({rotate: 180},{rotate: -180});
+  const [animate, cycle] = useCycle({rotate: 180}, {rotate: -180});
 
   function setClicked(id) {
     setQuestion(prevQuestion => {
@@ -38,7 +38,7 @@ export default function Quiz() {
 
   function getQuestion() {
     setIndex(index + 1);
-    if (index === queue.length-1) {
+    if (index === queue.length - 1) {
       const qs = questions.questions;
       let q = qs[qs.length * Math.random() << 0];
       while (used(q)) {
@@ -73,7 +73,10 @@ export default function Quiz() {
   const COG = () => {
     return (
         <motion.div className={"w-10 mr-[15vw] ml-auto mt-5 cursor-pointer"}
-                    animate={animate} transition={{bounce: 0, duration: 0.4}} onTap={() => {cycle(); onCycle();}}>
+                    animate={animate} transition={{bounce: 0, duration: 0.4}} onTap={() => {
+          cycle();
+          onCycle();
+        }}>
           <Cog8ToothIcon className={`w-10 h-10`}/>
         </motion.div>
     );
@@ -83,13 +86,12 @@ export default function Quiz() {
       <>
         <COG/>
         <p>Question: {index + 1} / {queue.length}</p>
-        <p className={"text-green-500"}>Correct: {correct} / {queue.length}</p>
-        <Prompt question={question} setClicked={setClicked}/>
         <div className={"fixed left-[50%] bottom-[15%]"}>
-        <HistorySidebar history={history}/>
-        <p>Question: {index+1} / {queue.length}</p>
-        <p>Correct: {correct} / {queue.length}</p>
-        <Prompt question={question} setClicked={setClicked} />
+          <HistorySidebar history={history}/>
+          <p>Question: {index + 1} / {queue.length}</p>
+          <p>Correct: {correct} / {queue.length}</p>
+        </div>
+        <Prompt question={question} setClicked={setClicked}/>
         <div className={"fixed left-[50%] bottom-[20%]"}>
           <div className={"grid grid-cols-2 relative left-[-50%] gap-x-32"}>
             {index === 0 ? <div/> : <ChevronLeftIcon
@@ -100,7 +102,7 @@ export default function Quiz() {
                     " border-2 w-60 h-10 cursor-pointer"} onClick={() => setQuestion(getQuestion())}/> : <div/>}
           </div>
         </div>
-        <SettingsSidebar isVisible={isVisible} />
+        <SettingsSidebar isVisible={isVisible}/>
       </>
   );
 }
