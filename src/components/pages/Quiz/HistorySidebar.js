b9b64bd7dfdddf106062783ab5e895a8a5a76717
Jake <jakezcanada@gmail.com>
2023-01-24 18:28:52 -0500

    Format history sidebar

--- src/components/pages/Quiz/HistorySidebar.js
+++ src/components/pages/Quiz/HistorySidebar.js
@@ -1,31 +1,32 @@
 export default function HistorySidebar(props) {
-    const content = props.history.map((hist, index) => {
-        const questionAnswer = hist.answers.find(ans => {
-            return ans.isRight
-        });
-        const userAnswer = hist.answers.find(ans => {
-            return ans.isClicked
-        });
-        console.log(hist);
-        return (
-            <li key={index}>
-                <div className="flex text-left p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">
-                    {hist.question}
-                    <br/>
-                    Answer: {questionAnswer.answer}
-                    <br/>
-                    Your answer: {userAnswer.answer}
-                </div>
-            </li>
-        );
+  const content = props.history.map((hist, index) => {
+    const questionAnswer = hist.answers.find(ans => {
+      return ans.isRight
     });
+    const userAnswer = hist.answers.find(ans => {
+      return ans.isClicked
+    });
+    console.log(hist);
     return (
-        <aside className="w-64" aria-label="Sidebar">
-            <div className="px-3 py-4 overflow-y-auto rounded bg-gray-50 dark:bg-gray-800">
-                <ul className="space-y-2">
-                    {content}
-                </ul>
-            </div>
-        </aside>
+      <li key={index}>
+        <div
+          className="flex text-left p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">
+          {hist.question}
+          <br/>
+          Answer: {questionAnswer.answer}
+          <br/>
+          Your answer: {userAnswer.answer}
+        </div>
+      </li>
     );
+  });
+  return (
+    <aside className="w-64" aria-label="Sidebar">
+      <div className="px-3 py-4 overflow-y-auto rounded bg-gray-50 dark:bg-gray-800">
+        <ul className="space-y-2">
+          {content}
+        </ul>
+      </div>
+    </aside>
+  );
 }
